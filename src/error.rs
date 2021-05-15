f92d2d19a0ab6230b5fd965d782639831ef67294
PhotonQuantum <self@lightquantum.me>
2021-05-16 00:01:53 +0800

    feat: derive more in Error

--- src/error.rs
+++ src/error.rs
@@ -2,7 +2,7 @@ use thiserror::Error;
 
 pub(crate) type Result<T> = std::result::Result<T, Error>;
 
-#[derive(Debug, Error)]
+#[derive(Debug, Error, Copy, Clone, Eq, PartialEq)]
 pub enum Error {
     #[error("Invalid index")]
     InvalidIndex,
